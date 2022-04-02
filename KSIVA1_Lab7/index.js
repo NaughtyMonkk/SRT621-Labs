e8e6bc27395941153ff7b7c41fb24d86a4f538a8
NaughtyMonkk <karthikdebest100@gmail.com>
2022-04-02 00:16:48 -0400

    commit lab7

---
+++ KSIVA1_Lab7/index.js
@@ -0,0 +1,16 @@
+
+const http = require("http");
+const host = 'localhost';
+const port = 8989;
+const requestListener = function (request,response){
+    var date = new Date()
+    let responseMessage = "<h1> Current Time and Date </h1>"+date;
+    response.write(responseMessage);
+    response.end();
+    console.log(`Sent a response : ${responseMessage}`);
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+
+});
